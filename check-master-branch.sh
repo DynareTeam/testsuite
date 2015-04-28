f637b664b35ff4e8ac8a8a471f897b8c11122c5a
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2015-04-28 13:05:09 +0200

    Send log files to web-server.

--- check-master-branch.sh
+++ check-master-branch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# WARNING: this script mirrors from kirikou. In particular, when a new stable
-# branch is created, the mirroring script on kirikou needs a manual update.
-
 set -ex
 
 LOGFILE=$(mktemp --tmpdir dynare-master-check-XXXXXXXXXX.log)
@@ -22,13 +19,45 @@ LAST_RAN_COMMIT=/home/dynbot/last-ran-testsuite-master.txt
     else
         RUN_TESTSUITE=1
         echo $COMMIT > $LAST_RAN_COMMIT
-        git submodule update --init
+	# Compile binaries (preprocessor and mex files)
         autoreconf -i -s
         ./configure --with-matlab=/usr/local/MATLAB/$MATLAB_VERSION MATLAB_VERSION=$MATLAB_VERSION
-        make -j5 all
+        make -j8 all
         # Don't fail at errors in the testsuite
         set +e
+	# Run tests (matlab and octave)
         make -C tests -j8 check
+	cd tests
+	# Copy the generated log files...
+	cp --parents `find -name \*.m.log` ../tests.logs.m
+	cp --parents `find -name \*.o.log` ../tests.logs.o
+	cd ..
+	# ... and send them on kirikou.
+	rsync -az tests.logs.m/* kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/matlab
+	rsync -az tests.logs.o/* kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/octave	
+	# Write and send footers
+	{
+	    echo "# Matlab testsuite (master branch)"
+	    echo "Commit [$(git log --pretty=format:'%h' -n 1)](https://github.com/DynareTeam/dynare/commit/$(git log --pretty=format:'%H' -n 1)) by $(git log --pretty=format:'%an' -n 1) [$(git log --pretty=format:'%ad' -n 1)]"
+	} > header.md
+	pandoc header.md -o header.html
+	scp header.html kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/matlab/header.html
+	rm header.*
+	{
+	    echo "# Octave testsuite (master branch)"
+	    echo "Commit [$(git log --pretty=format:'%h' -n 1)](https://github.com/DynareTeam/dynare/commit/$(git log --pretty=format:'%H' -n 1)) by $(git log --pretty=format:'%an' -n 1) [$(git log --pretty=format:'%ad' -n 1)]"
+	} > header.md
+	pandoc header.md -o header.html
+	scp header.html kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/octave/header.html
+	rm header.*
+	# Write and send footers
+	{
+	    echo "Produced by dynbot on karaba $(date)."
+	} > footer.md
+	pandoc footer.md -o footer.html
+	scp footer.html kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/matlab/footer.html
+	scp footer.html kirikou.cepremap.org:/srv/d_kirikou/www.dynare.org/testsuite/master/octave/footer.html
+	rm footer.*
         set -e
     fi
 } >$LOGFILE 2>&1
@@ -37,21 +66,13 @@ if [[ $RUN_TESTSUITE == 0 ]]; then
     rm -f $LOGFILE
 else
     chmod +r $LOGFILE
-
     {
         cd $TMP_DIR/dynare && git log -1 --pretty=oneline HEAD
         echo
         cat $RESULTS_MATLAB || echo -e "Dynare failed to compile or MATLAB testsuite failed to run\n"
         cat $RESULTS_OCTAVE || echo -e "Dynare failed to compile or Octave testsuite failed to run\n"
-        echo "A full log can be found on karaba in '$LOGFILE'."
+        echo "A full log can be found at http://www.dynare.org/testsuite/master"
     } | mail -s "Status of testsuite in master branch" dev@dynare.org -aFrom:"Dynare Robot <dynbot@dynare.org>"
-#-- -f "Dynare Robot <dynbot@dynare.org>"
 fi
 
 rm -rf $TMP_DIR
-
-
-
-
-
-git clone --depth 1 --recursive --branch 4.4 --single-branch git@github.com:DynareTeam/dynare.git
