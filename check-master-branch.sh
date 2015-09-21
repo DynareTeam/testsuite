0332eda7edffeb0295fea7b7774456ce84442b8e
Houtan Bastani <houtan@dynare.org>
2015-09-21 12:07:31 +0200

    fix path to timing script

--- check-master-branch.sh
+++ check-master-branch.sh
@@ -95,7 +95,7 @@ LAST_RAN_COMMIT=$TESTSUITE_CODE_PATH/last-ran-testsuite-master.txt
 	tar -jcvf octavelogs.tar.bz2 $TMP_DIR/dynare/tests.logs.m
 	scp *.tar.bz2 $SERVER_PATH
 	# Update timing, create index, copy to kirikou
-        ./timing-and-html-file.sh $TMP_DIR/dynare/tests
+        $TESTSUITE_CODE_PATH/timing-and-html-file.sh $TMP_DIR/dynare/tests
         scp $TESTSUITE_CODE_PATH/../testSuiteTiming/* $SERVER_PATH/timing/
         set -e
     fi
