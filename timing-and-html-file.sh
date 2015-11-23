a444a43c224fbf5349eab3d1a2260ee5fc5818c5
Houtan Bastani <houtan@dynare.org>
2015-11-23 16:09:37 +0100

    incorporate timing.py into testsuite

--- timing-and-html-file.sh
+++ timing-and-html-file.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-TESTSUITE_CODE_PATH=$(dirname $(realpath -s $0))
-CSVPATH=$(realpath $TESTSUITE_CODE_PATH/../testSuiteTiming)
-HTML=$CSVPATH/index.html
+set -ex
+
+TESTSUITE_CODE_PATH=/home/houtanb/TESTSUITE/testsuite
+CSVPATH=/home/houtanb/TESTSUITE/testSuiteTiming
 ORIGPATH=$PWD
 DATE=`date +%Y%m%d`
 
@@ -20,39 +21,13 @@ for file in $TRS_FILES; do
     csvfile=`echo $file | sed 's/\//-/g' | sed 's/\.trs$/\.csv/g'`
     if [ ! -f $CSVPATH/$csvfile ]; then
         name=`echo $file | sed 's/\.m\.trs$/.mod/g' | sed 's/\.o\.trs$/.mod/g'`
-        echo "DATE,$name,SHA" > $CSVPATH/$csvfile
+        echo "Date,$name,SHA" > $CSVPATH/$csvfile
     fi
     echo $DATE,$time,$SHA >> $CSVPATH/$csvfile
 done
 
 # Create html file for graphs
 cd $CSVPATH
-echo "<html>"     > $HTML
-echo "<head>"    >> $HTML
-echo "<script type=\"text/javascript\" src=\"http://cdnjs.cloudflare.com/ajax/libs/dygraph/1.1.0/dygraph-combined.js\"></script>"    >> $HTML
-echo "</head>"   >> $HTML
-echo "<body>"    >> $HTML
-
-COUNTER=1
-for file in *.csv
-do
-    title=`echo $file | sed -e 's/-/\//' | sed 's/\.o\.csv/\.mod/g' | sed 's/\.m\.csv/\.mod/g'`
-    echo "<div id=\"graphdiv$COUNTER\" style=\"width:600px; height:180px;\"></div>" >> $HTML
-    echo "<script type=\"text/javascript\">"                                        >> $HTML
-    echo " g$COUNTER = new Dygraph("                                                >> $HTML
-    echo "    document.getElementById(\"graphdiv$COUNTER\"),"                       >> $HTML
-    echo "    \"$file\","              >> $HTML
-    echo "    {"                       >> $HTML
-    echo "     ylabel: 'cputime',"     >> $HTML
-    echo "     title: '$title',"       >> $HTML
-    echo "    }"                       >> $HTML
-    echo " );"                         >> $HTML
-    echo "</script>"                   >> $HTML
-    COUNTER=$((COUNTER + 1))
-done
-
-echo "</body>"    >> $HTML
-echo "</html>"    >> $HTML
-
+python $TESTSUITE_CODE_PATH/timing.py
 cd $ORIGPATH
 
