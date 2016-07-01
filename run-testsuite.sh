DynareTeam/testsuite

commit 8dd0f4383bd0cf65089ead754262ec8606191afa

make testsuite work with http repository as well

run-testsuite.sh
@@ -46,6 +46,9 @@ LAST_RAN_COMMIT=$TESTSUITE_CODE_PATH/last-ran-testsuite-$GIT_BRANCH.txt
 {
     cd $TMP_DIR
     git clone --depth 1 --recursive --branch $GIT_BRANCH --single-branch $GIT_REPOSITORY_SSH
+    if [ ! -d "dynare" ]; then
+        git clone --depth 1 --recursive --branch $GIT_BRANCH --single-branch $GIT_REPOSITORY_HTTP
+    fi
     cd dynare
     COMMIT=$(git log -1 --pretty=oneline HEAD)
     if [[ -f $LAST_RAN_COMMIT && "$(cat $LAST_RAN_COMMIT)" == "$(echo $COMMIT)" ]]; then

configure.inc.example
@@ -14,8 +14,10 @@ MATLAB_PATH=/usr/local/MATLAB
 
 ## Set branch name to be tested
 GIT_BRANCH=master
- 
-## Set git repository ($USER needs to have an account on Github, identified by a valid ssh key)
+
+## Set the git repository
+##    If the $USER has an account on Github (w SSH key uploaded), the SSH repository will be used
+##    Otherwise, the HTTP repository will be used
 GIT_REPOSITORY_SSH=git@github.com:DynareTeam/dynare.git
 GIT_REPOSITORY_HTTP=https://github.com/DynareTeam/dynare
 

commit f6668dbbf674c87a263511bd9862eea7d468f44f

fix bug introduced in: 8dd0f4383bd0cf65089ead754262ec8606191afa

run-testsuite.sh
@@ -45,7 +45,10 @@ LAST_RAN_COMMIT=$TESTSUITE_CODE_PATH/last-ran-testsuite-$GIT_BRANCH.txt
 
 {
     cd $TMP_DIR
+    # Don't fail when attempting to clone via SSH
+    set +e
     git clone --depth 1 --recursive --branch $GIT_BRANCH --single-branch $GIT_REPOSITORY_SSH
+    set -e
     if [ ! -d "dynare" ]; then
         git clone --depth 1 --recursive --branch $GIT_BRANCH --single-branch $GIT_REPOSITORY_HTTP
     fi